b937f38d288dc3eeb000292a7b324c4515f9cfa5
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-11-23 17:02:52 -0200

    :heavy_plus_sign: Add django debug toolbar
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/project_name/settings.py
+++ src/project_name/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -29,6 +31,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 
@@ -47,6 +51,7 @@ TEMPLATES = [
         },
     },
 ]
+INTERNAL_IPS = ['127.0.0.1']
 
 DATABASES = {
     'default': {
